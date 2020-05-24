1f290a6722c446acecd279c75ebdd529b3d6dd9c
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-05-24 10:31:48 +0530

    server

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ const app = express();
 
 const PORT = process.env.PORT | 4000;
 
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
 app.listen(PORT, () => {
   console.log(`Server Started at port ${PORT}`);
 });
